f950713c51007ab7d1549a18813711bf5f17dcf7
bpieczek <92160230+bpieczek@users.noreply.github.com>
2022-10-19 14:16:51 +0200

    Update and rename eryk.js to dziala.js

---
+++ commands/dziala.js
@@ -0,0 +1,11 @@
+const log = console.log
+
+module.exports = {
+    name: "dziala",
+    description: "Odsyła wiadomość o dzialaniu bota",
+    template: "dziala",
+    run(msg, args)
+    {
+      msg.reply("Działa!")
+    }
+}
